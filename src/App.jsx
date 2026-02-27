6e55619268e659e0d070742f1ed6ee34046aad58
Andrew De La Torre <drewdlt@yahoo.com>
2026-02-26 18:10:33 -0800

    Lift cart up and share it between pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,23 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import CheckoutPage from "./pages/checkout/CheckoutPage";
 import OrdersPage from "./pages/OrdersPage";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/cart-items")
+    .then((response) => {
+      setCart(response.data);
+    });
+  }, []);
+
   return (
     <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="checkout" element={<CheckoutPage />} />
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage />} />
     </Routes>
   );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,19 +4,14 @@ import Header from "../components/Header";
 import "./HomePage.css";
 // import { products } from "../../starting-code/data/products";
 
-export default function HomePage() {
+export default function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
+  
 
   useEffect(() => {
     axios.get("/api/products")
     .then((response) => {
       setProducts(response.data);
-    });
-
-    axios.get("/api/cart-items")
-    .then((response) => {
-      setCart(response.data);
     });
   }, []);
 
