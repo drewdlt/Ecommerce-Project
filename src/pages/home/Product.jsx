839cd7a8d4912e844fb3bd4e448fe8fb6e0a7354
Andrew De La Torre <drewdlt@yahoo.com>
2026-02-27 18:04:59 -0800

    Create integration test for Product component

--- src/pages/home/Product.jsx
+++ src/pages/home/Product.jsx
@@ -16,7 +16,7 @@ export function Product({ product, loadCart }) {
 
     setTimeout(() => {
       setShowAddedMessage(false);
-    }, 2000)
+    }, 2000);
 
     await loadCart();
   };
@@ -29,7 +29,11 @@ export function Product({ product, loadCart }) {
   return (
     <div className="product-container">
       <div className="product-image-container">
-        <img className="product-image" src={product.image} />
+        <img
+          className="product-image"
+          data-testid="product-image"
+          src={product.image}
+        />
       </div>
 
       <div className="product-name limit-text-to-2-lines">{product.name}</div>
@@ -37,6 +41,7 @@ export function Product({ product, loadCart }) {
       <div className="product-rating-container">
         <img
           className="product-rating-stars"
+          data-testid="product-rating-stars-image"
           src={`images/ratings/rating-${product.rating.stars * 10}.png`}
         />
         <div className="product-rating-count link-primary">
@@ -63,7 +68,10 @@ export function Product({ product, loadCart }) {
 
       <div className="product-spacer"></div>
 
-      <div className="added-to-cart" style={{opacity: showAddedMessage ? 1 : 0}}>
+      <div
+        className="added-to-cart"
+        style={{ opacity: showAddedMessage ? 1 : 0 }}
+      >
         <img src="images/icons/checkmark.png" />
         Added
       </div>
